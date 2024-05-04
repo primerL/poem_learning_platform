f0efdb47636c964a471a1b0242fa4112bbd743e3
LB <libo81501@gmail.com>
2024-05-04 14:56:29 +0800

    测试socket.io和axios正常请求

--- poem-learning-platform/src/views/LoginPage.vue
+++ poem-learning-platform/src/views/LoginPage.vue
@@ -2,10 +2,12 @@
 </template>
 <script setup>
 import { onMounted } from 'vue';
+import io from 'socket.io-client';
 import axios from 'axios';
 
-
 onMounted(() => {
+
+    // axios请求
     let instance = axios.create({
         baseURL: 'http://127.0.0.1:4523/m1/4199717-0-default',
     })
@@ -13,6 +15,13 @@ onMounted(() => {
     instance.get('/test').then(res => {
         console.log(res.data);
     })
+
+    // io
+    const socket = io('ws://127.0.0.1:2002');
+    socket.on('connect', () => {
+        console.log('connect');
+    });
+
 });
 
 

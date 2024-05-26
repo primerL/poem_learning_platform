20e2c94d8d19de18b6ef5a9340278021be05a3d1
LB <libo81501@gmail.com>
2024-05-26 18:16:56 +0800

    修改room

--- poetry-conference/src/main/java/edu/fudan/poetryconference/controller/RoomController.java
+++ poetry-conference/src/main/java/edu/fudan/poetryconference/controller/RoomController.java
@@ -6,23 +6,26 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 @RestController
-@RequestMapping("/rooms")
+@RequestMapping("/api/rooms")
 public class RoomController {
     @Autowired
     private RoomService roomService;
 
+    @CrossOrigin
     @PostMapping("/{roomId}/increment")
     public ResponseEntity<String> incrementRoomCount(@PathVariable String roomId) {
         roomService.incrementRoomCount(roomId);
         return ResponseEntity.ok("success");
     }
 
+    @CrossOrigin
     @PostMapping("/{roomId}/decrement")
     public ResponseEntity<String> decrementRoomCount(@PathVariable String roomId) {
         roomService.decrementRoomCount(roomId);
         return ResponseEntity.ok("success");
     }
 
+    @CrossOrigin
     @GetMapping("/{roomId}/count")
     public ResponseEntity<Integer> getRoomCount(@PathVariable String roomId) {
         int count = roomService.getRoomCount(roomId);
